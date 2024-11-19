5682b3786227060c264d0283c2081e7299f80bb7
vr121170 <82792543+vr121170@users.noreply.github.com>
2024-11-19 11:40:21 +0900

    initial

--- findMe/settings.py
+++ findMe/settings.py
@@ -36,7 +36,7 @@ SECRET_KEY = 'django-insecure-xd!=^$=cff^npv)dv(8@(%=3@=pox2j#+(h6@(i3=m!#zxpgb9
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 # Application definition
 
