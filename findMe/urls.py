08ed3042f511faf413a654f2b6660db0c6a115b8
vr121170 <82792543+vr121170@users.noreply.github.com>
2024-11-19 11:37:42 +0900

    initial

--- findMe/urls.py
+++ findMe/urls.py
@@ -15,13 +15,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
-import main
-import users
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('main/', main.urls),
-    path('users/', users.urls),
+    path('main/', include("main.urls")),
+    path('users/', include("users.urls")),
 ]
